27a34fc72e266b66fba7135daf6375830bea3ca3
ncameiri <74956154+ncameiri@users.noreply.github.com>
2022-02-24 17:40:25 +0000

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,12 @@
-useradd -m teste;
-service start ssh;
-printf '1234\n1234\n' |passwd teste;
-printf 'y\n' | apt install ssh;
-service ssh start;	
-sudo usermod -a -G sudo teste;
+#useradd -m teste;
+#service start ssh;
+#printf '1234\n1234\n' |passwd teste;
+#printf 'y\n' | apt install ssh;
+#service ssh start;	
+#sudo usermod -a -G sudo teste;
+#tail -f /dev/null;
+printf '123456789\n123456789\n'| adduser -g "teste" teste;
+apk add openssh;
+ssh-keygen -A
+/usr/sbin/sshd
 tail -f /dev/null;
