971518159383f4808d5063d35bfe024e1dfb195f
ncameiri <74956154+ncameiri@users.noreply.github.com>
2022-02-24 17:44:51 +0000

    Update init.sh

--- init.sh
+++ init.sh
@@ -7,6 +7,8 @@
 #tail -f /dev/null;
 printf '123456789\n123456789\n'| adduser -g "teste" teste;
 apk add openssh;
+apk add sudo;
+sudo usermod -a -G sudo teste;
 ssh-keygen -A
 /usr/sbin/sshd
 tail -f /dev/null;
